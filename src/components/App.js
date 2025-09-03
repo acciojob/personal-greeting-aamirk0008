84836b727291c77e2a763ae2d57a7439b2c53977
Sheikh Aamir <113338901+aamirk0008@users.noreply.github.com>
2025-09-03 15:43:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 
 import React from "react";
+import { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [task, setTask] = useState('')
+
+  const handleInput = (e) => {
+    let value = e.target.value
+    setTask(value)
+  }
   return (
     <div>
+    <input value={task} type='text' onChange={handleInput} placeholder="Enter Name"/>
+    {task ? <p>Hello {task}</p> : ''}
         {/* Do not remove the main div */}
     </div>
   )
